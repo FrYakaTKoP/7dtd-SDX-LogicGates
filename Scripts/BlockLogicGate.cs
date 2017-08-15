1ee46934f79c7e098999f4caeff43126d40bde92
FrYakaTKoP <fryakatkop@gmail.com>
2017-08-16 00:36:30 +0200

    Signed-off-by: FrYakaTKoP

--- Scripts/BlockLogicGate.cs
+++ Scripts/BlockLogicGate.cs
@@ -6,34 +6,174 @@ using System.IO;
 
 public class BlockLogicGate : BlockPowered
 {
-    // does get called at Gamestartup
+	private bool showDebugLog = true;
+	
+	public void DebugMsg(string msg)
+	{
+		if(showDebugLog)
+		{
+			Debug.Log(msg);
+		}
+	}
+	
+    // get called at Gamestartup
     public override void Init()
 	{
-		base.Init();        
-		Debug.Log("BlockLogicGate.init");
+		base.Init();  
+		// TODO: write fancy "mod Loaded" log		
+		DebugMsg("BlockLogicGate.init");
+	}
+
+	
+	// get called on first wire hookup  
+    // doesn't care if wire is powered (even from a switch without powersource)
+	// BlockPowered
+	public override TileEntityPowered CreateTileEntity(Chunk chunk)
+	{
+		DebugMsg("BlockLogicGate.CreateTileEntity");
+		return new TileEntityPoweredBlock(chunk);
+	}
+	/*
+	public override TileEntityPowered CreateTileEntity(Chunk chunk)
+	{
+		DebugMsg("BlockLogicGate.CreateTileEntity");
+	    return new TileEntityPoweredBlock(chunk)
+		{
+			PowerItemType = PowerItem.PowerItemTypes.Consumer
+        };
+	}
+	*/
+
+	
+	// Gets Called on lc pickup or on destroy/fall
+	// BlockPowered
+	public override void OnBlockRemoved(WorldBase world, Chunk _chunk, Vector3i _blockPos, BlockValue _blockValue)
+	{
+		DebugMsg("BlockLogicGate.onBlockRemoved");
+		base.OnBlockRemoved(world, _chunk, _blockPos, _blockValue);
+		if (_blockValue.ischild)
+		{
+			DebugMsg("BlockLogicGate.onBlockRemoved _blockValue.ischild");
+			while (true)
+			{
+				switch (7)
+				{
+				case 0:
+					continue;
+				}
+				break;
+			}
+			if (!true)
+			{
+				// methodof error
+				//RuntimeMethodHandle arg_26_0 = methodof(BlockPowered.OnBlockRemoved(WorldBase, Chunk, Vector3i, BlockValue)).MethodHandle;
+			}
+			return;
+		}
+		TileEntityPowered tileEntityPowered = _chunk.GetTileEntity(World.toBlock(_blockPos)) as TileEntityPowered;
+		// don't know why but tileEntityPowered is null here but shouldn't 
+		if (tileEntityPowered != null)
+		{
+			// not executed even when powered
+			DebugMsg("BlockLogicGate.onBlockRemoved tileEntityPowered != null");
+			while (true)
+			{
+				switch (2)
+				{
+				case 0:
+					continue;
+				}
+				break;
+			}
+			if (!GameManager.IsDedicatedServer)
+			{
+				EntityPlayerLocal primaryPlayer = GameManager.Instance.World.GetPrimaryPlayer();
+				if (primaryPlayer.inventory.holdingItem.Actions[1] is ItemActionConnectPower)
+				{
+					while (true)
+					{
+						switch (4)
+						{
+						case 0:
+							continue;
+						}
+						break;
+					}
+					// GX is private -> see ItemActionConnectPower
+					//(primaryPlayer.inventory.holdingItem.Actions[1] as ItemActionConnectPower).GX(primaryPlayer, _blockPos);
+				}
+			}
+			if (Steam.Network.IsServer)
+			{
+				while (true)
+				{
+					switch (1)
+					{
+					case 0:
+						continue;
+					}
+					break;
+				}
+				PowerManager.Instance.RemovePowerNode(tileEntityPowered.GetPowerItem());
+			}
+			if (tileEntityPowered.GetParent().y != -9999)
+			{
+				DebugMsg("BlockLogicGate.onBlockRemoved tileEntityPowered.GetParent().y != -9999");
+				while (true)
+				{
+					switch (5)
+					{
+					case 0:
+						continue;
+					}
+					break;
+				}
+				IPowered powered = world.GetTileEntity(0, tileEntityPowered.GetParent()) as IPowered;
+				if (powered != null)
+				{
+					while (true)
+					{
+						switch (5)
+						{
+						case 0:
+							continue;
+						}
+						break;
+					}
+					if (Steam.Network.IsServer)
+					{
+						while (true)
+						{
+							switch (5)
+							{
+							case 0:
+								continue;
+							}
+							break;
+						}
+						powered.SendWireData();
+					}
+				}
+			}
+			tileEntityPowered.RemoveWires();
+		}
+		_chunk.RemoveTileEntityAt<TileEntityPowered>((World)world, World.toBlock(_blockPos));
 	}
     
     // don't get called
     public override void OnBlockLoaded(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
 	{
 		base.OnBlockLoaded(_world, _clrIdx, _blockPos, _blockValue);        
-		Debug.Log("BlockLogicGate.onBlockLoaded");
+		DebugMsg("BlockLogicGate.onBlockLoaded");
 	}  
-	    
-	public override TileEntityPowered CreateTileEntity(Chunk chunk)
-	{
-		Debug.Log("BlockLogicGate.CreateTileEntity");
-        return new TileEntityPoweredBlock(chunk)
-		{
-			PowerItemType = PowerItem.PowerItemTypes.Consumer
-        };
-	}
+	
+
     
-   /*
+	// don't get called 
     public override void OnBlockEntityTransformAfterActivated(WorldBase _world, Vector3i _blockPos, int _cIdx, BlockValue _blockValue, BlockEntityData _ebcd)
 	{
 		base.OnBlockEntityTransformAfterActivated(_world, _blockPos, _cIdx, _blockValue, _ebcd);        
-		Debug.Log("BlockLogicGate.OnBlockEntityTransformAfterActivated");
+		DebugMsg("BlockLogicGate.OnBlockEntityTransformAfterActivated");
 		if (_blockValue.ischild)
 		{
 			while (true)
@@ -49,6 +189,7 @@ public class BlockLogicGate : BlockPowered
 			{
                 // trows Compiler Error CS1525
 				//RuntimeMethodHandle arg_2A_0 = methodof(BlockPowered.OnBlockEntityTransformAfterActivated(WorldBase, Vector3i, int, BlockValue, BlockEntityData)).MethodHandle;
+				
 			}
 			return;
 		}
@@ -57,7 +198,7 @@ public class BlockLogicGate : BlockPowered
 		if (tileEntityPowered != null)
 		{
 			tileEntityPowered.BlockTransform = _ebcd.transform;
-			GameManager.Instance.StartCoroutine(this.OE(tileEntityPowered));
+			//GameManager.Instance.StartCoroutine(this.OE(tileEntityPowered));
 			if (tileEntityPowered.GetParent().y != -9999)
 			{
 				while (true)
@@ -72,14 +213,22 @@ public class BlockLogicGate : BlockPowered
 				IPowered powered = _world.GetTileEntity(0, tileEntityPowered.GetParent()) as IPowered;
 				if (powered != null)
 				{
-					GameManager.Instance.StartCoroutine(this.OE(powered));
+					DebugMsg("BlockLogicGates.OnBlockEntityTransformAfterActivated powered != null ");
+					//GameManager.Instance.StartCoroutine(this.OE(powered));
 				}
 			}
 		}
 
 	}
-    
- 
+	
+	// don't get called
+	public override void OnBlockEntityTransformBeforeActivated(WorldBase _world, Vector3i _blockPos, int _cIdx, BlockValue _blockValue, BlockEntityData _ebcd)
+	{
+		this.shape.OnBlockEntityTransformBeforeActivated(_world, _blockPos, _cIdx, _blockValue, _ebcd);
+		DebugMsg("BlockLogicGate.OnBlockEntityTransformBeforeActivated");
+	}
+	
+	/* 
     // throws error CS0305 
     // BlockPowered
     private IEnumerator OE(IPowered powered)
@@ -90,6 +239,7 @@ public class BlockLogicGate : BlockPowered
 		return dT;
 	}
 
+	
 	public override void OnBlockEntityTransformBeforeActivated(WorldBase _world, Vector3i _blockPos, int _cIdx, BlockValue _blockValue, BlockEntityData _ebcd)
 	{
 		this.shape.OnBlockEntityTransformBeforeActivated(_world, _blockPos, _cIdx, _blockValue, _ebcd);
@@ -103,7 +253,7 @@ public class BlockLogicGate : BlockPowered
 		script.blockPos = _blockPos;
 		script.cIdx = _cIdx;
 		script.ebcd = _ebcd;
-		Debug.Log("BlockLogicGate.OnBlockEntityTransformBeforeActivated");
+		DebugMsg("BlockLogicGate.OnBlockEntityTransformBeforeActivated");
 	}	
    */
 }
