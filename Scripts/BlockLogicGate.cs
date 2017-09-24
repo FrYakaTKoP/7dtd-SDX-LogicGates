e0b83d851958eead6d9af3644935fb32c8231bb4
FrYakaTKoP <fryakatkop@gmail.com>
2017-09-24 02:05:06 +0200

    - cleanup  - added outputMode (activeLow)  - changed position of ouputMode indicator in model

--- Scripts/BlockLogicGate.cs
+++ Scripts/BlockLogicGate.cs
@@ -9,6 +9,9 @@ using System.Diagnostics;
 public class BlockLogicGateMain : BlockPowered
 {	
     private static bool showDebugLog = true;
+	private int metaIndexPowered = 0;
+	private int metaIndexTriggered = 1;
+	private int metaIndexOutMode = 2;
 
 	public static void DebugMsg(string msg)
 	{
@@ -28,21 +31,13 @@ public class BlockLogicGateMain : BlockPowered
     // gets called onFocus of the switch 
 	public override string GetActivationText(WorldBase _world, BlockValue _blockValue, int _clrIdx, Vector3i _blockPos, EntityAlive _entityFocusing)
 	{
-        //string msg = String.Concat("GetActivationText - _blockValue.meta &2 :", (_blockValue.meta & 2));
-        //DebugMsg(msg);
-        // (_blockValue.meta &2):
-        // 0 : switch is Off , 1 : switch is On
-    
 		PlayerActionsLocal playerInput = ((EntityPlayerLocal)_entityFocusing).playerInput;
 		string keybindString = UIUtils.GetKeybindString(playerInput.Activate, playerInput.PermanentActions.Activate);
-        if ((_blockValue.meta & 2) != 0)
+        if (getBitBool(_blockValue.meta, metaIndexOutMode))
 		{
-
-			//return string.Format(Localization.Get("useSwitchLightOff", string.Empty), keybindString);
-			return string.Format("cSwitchOff", keybindString);
+			return "<E> toggle out Active High";
 		}
-		//return string.Format(Localization.Get("useSwitchLightOn", string.Empty), keybindString);
-        return string.Format("cSwitchOn", keybindString);
+        return  "<E> toggle out Active Low";
     }
     
 	// calls when player use the block
@@ -51,11 +46,8 @@ public class BlockLogicGateMain : BlockPowered
         DebugMsg("OnBlockActivated");
 		DebugMsg(String.Concat("OnBlockActivated  _indexInBlockActivationCommands=", _indexInBlockActivationCommands));
 
-			
-		
 		StackTrace st = new StackTrace(1, true);
 		StackFrame [] stFrames = st.GetFrames();
-
 		for(int i=0; i < stFrames.Length; i++ )
 		{
 		   DebugMsg(String.Concat("OnBlockActivated CallingMethod:",stFrames[i].GetMethod() ));
@@ -76,19 +68,71 @@ public class BlockLogicGateMain : BlockPowered
 			{
 				return false;
 			}
-			this.XR(_world, _cIdx, _blockPos, _blockValue, true);
+			ToggleOutputMode(_world, _cIdx, _blockPos, _blockValue);
+			//this.XR(_world, _cIdx, _blockPos, _blockValue);
 			return true;
 		}
 	}
+	
+	private void ToggleOutputMode(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue)
+	{
+        DebugMsg("ToggleOutputMode");
+		_blockValue.meta = flipBit(_blockValue.meta, metaIndexOutMode); //^= (1 << 3); //8;
+		
+		bool newMode = getBitBool(_blockValue.meta, metaIndexOutMode);
+		DebugMsg(String.Concat("ToggleOutputMode newMode=", newMode ? "1" : "0"));
+		
+		bool BlockIsTriggered = getBitBool(_blockValue.meta, metaIndexTriggered); 
+		bool newIsTriggered = false;
+			
+		TileEntityPoweredTrigger tileEntityPoweredTrigger = _world.GetTileEntity(_cIdx, _blockPos) as 	TileEntityPoweredTrigger;
+		if (tileEntityPoweredTrigger != null)
+		{
+			if(BlockIsTriggered)
+			{	
+				newIsTriggered = !newMode;				
+			}
+			else
+			{
+				newIsTriggered = newMode;
+			}			
+			tileEntityPoweredTrigger.IsTriggered = newIsTriggered;
+			_blockValue.meta = setBoolBit(_blockValue.meta, 1, newIsTriggered); 
+		}
+		_world.SetBlockRPC(_cIdx, _blockPos, _blockValue);			
+		this.XR(_world, _cIdx, _blockPos, _blockValue);
+	}
+
+	private byte flipBit(byte b, int i)
+	{
+		b ^= (byte)(1 << i);
+		return b;
+	}
+	
+	private bool getBitBool(byte b, int i)
+	{
+		int mask = 1 << i;
+		return (b & mask) != 0;
+	}
+	
+		private byte setBoolBit(byte b, int i, bool val)
+        {
+            byte mask = (byte)(1 << i);
+            if (val)
+            {
+                b |= mask;
+            }
+            else if(!val)
+            {
+                b &= (byte)~mask; 
+            }
+            return b;
+        }
 
 	// 
-	private bool XR(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, bool flagOnBlockActivated)
+	private bool XR(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue)
 	{
         DebugMsg("XR");
-        if(flagOnBlockActivated == null)
-        {
-            flagOnBlockActivated = false;
-        }
 		ChunkCluster chunkCluster = _world.ChunkClusters[_cIdx];
 		if (chunkCluster == null)
 		{
@@ -101,16 +145,18 @@ public class BlockLogicGateMain : BlockPowered
 
 		bool[] inputStates = GetInputStates(_world, _cIdx, _blockPos, _blockValue);
 		
-		bool BlockIsPowered = (_blockValue.meta & 1) != 0;
-		bool BlockIsTriggered = (_blockValue.meta & 2) != 0;
+		bool BlockIsPowered = getBitBool(_blockValue.meta, metaIndexPowered); //(_blockValue.meta & 1) != 0;
+		bool BlockIsTriggered = getBitBool(_blockValue.meta, metaIndexTriggered); //(_blockValue.meta & 2) != 0;		
+		bool outputMode =  getBitBool(_blockValue.meta, metaIndexOutMode); //(_blockValue.meta & 4) != 0; 
 		
-		
-		DebugMsg(String.Concat("XR BlockMetaIsPowered=", BlockIsPowered  ? "1" : "0"));
-		DebugMsg(String.Concat("XR BlockMetaIsTriggered=", BlockIsTriggered  ? "1" : "0"));
+		DebugMsg(String.Concat("XR _blockvalue.meta=", Convert.ToString(_blockValue.meta, 2).PadLeft(8, '0')));
+		DebugMsg(String.Concat("XR BlockMetaPowered=", BlockIsPowered  ? "1" : "0"));
+		DebugMsg(String.Concat("XR BlockMetaTriggered=", BlockIsTriggered  ? "1" : "0"));
+		DebugMsg(String.Concat("XR outputMode=", outputMode  ? "1" : "0"));
 
 		DebugMsg(String.Concat("XR _blockValue.rotation=", _blockValue.rotation));
 		
-        flagOnBlockActivated = false; // this disables toggle
+        bool flagOnBlockActivated = false; // this disables toggle
 		if (flagOnBlockActivated)
 		{
 			BlockIsTriggered = !BlockIsTriggered;
@@ -131,136 +177,132 @@ public class BlockLogicGateMain : BlockPowered
 			// }			
 			DebugMsg(String.Concat("XR -> tEPT.IsTriggered2=", tileEntityPoweredTrigger.IsTriggered ? "1" : "0"));
 		}
-		// 		
-		TileEntityPowered tileEntityPowered = _world.GetTileEntity(_cIdx, _blockPos) as TileEntityPowered;
-		if(tileEntityPowered != null)
-		{				
-			PowerItem powerItem = tileEntityPowered.GetPowerItem() as PowerItem;
-			if(powerItem != null)
-			{
-				// crashes the game 
-				//powerItem.HandlePowerUpdate(BlockIsTriggered);
-			}
-			
-			PowerTrigger powerTrigger = tileEntityPowered.GetPowerItem() as PowerTrigger;
-			if(powerTrigger != null)
-			{		
-				// crashes the game 
-				//powerTrigger.HandlePowerUpdate(BlockIsTriggered);
-			}         
-            
-			DebugMsg(String.Concat("XR -> tEP.ChildCount=", tileEntityPowered.ChildCount));
-			DebugMsg(String.Concat("XR -> tEP.isPowered=", tileEntityPowered.IsPowered ? "1" : "0"));
-            // 
-            //tileEntityPowered.;
-			
-			// not sure
-			//tileEntityPowered.CheckForNewWires();
-			
-			// don't do anything noticeable
-			//tileEntityPowered.MarkChanged();
-            
-            // don't do anything noticeable
-            //tileEntityPowered.CheckForNewWires();
-		}
-        
-    /*     
-        TileEntity tileEntity = _world.GetTileEntity(_cIdx, _blockPos) as 	TileEntity;
-		if (tileEntity != null)
-		{
-            // don't do anything noticeable
-			//tileEntity.SetModified();
-        }
-      */   
-        
 		
+		/* 		
+			TileEntityPowered tileEntityPowered = _world.GetTileEntity(_cIdx, _blockPos) as TileEntityPowered;
+			if(tileEntityPowered != null)
+			{				
+				DebugMsg(String.Concat("XR -> tEP.ChildCount=", tileEntityPowered.ChildCount));
+				DebugMsg(String.Concat("XR -> tEP.isPowered=", tileEntityPowered.IsPowered ? "1" : "0"));
+			} 
+		*/
 		
 		BlockEntityData _ebcd = ((World)_world).ChunkClusters[_cIdx].GetBlockEntity(_blockPos);
 		if (_ebcd != null && _ebcd.transform != null && _ebcd.transform.gameObject != null)
 		{	
-            // only works once per world
-            //GameObject IndicatorsObj = GameObject.Find("Indicators").gameObject;
-            
-            GameObject IndicatorsObj = _ebcd.transform.Find("Indicators").gameObject;			
+			UpdateIndicators(_ebcd, inputStates, BlockIsPowered, BlockIsTriggered, outputMode);
+		}		
+		return true;
+	}
+	
+	private void UpdateIndicators(BlockEntityData _ebcd, bool[] inputStates, bool BlockIsPowered, bool BlockIsTriggered, bool outputMode)
+	{
+        DebugMsg("UpdateIndicators");
+		GameObject IndicatorsObj = _ebcd.transform.Find("Indicators").gameObject;			
 
-			if(IndicatorsObj == null)
+		if(IndicatorsObj == null)
+		{
+			DebugMsg("IndicatorsObj is null");
+		}
+		Transform[] Indicators = IndicatorsObj.GetComponentsInChildren<Transform>();
+		if (Indicators != null)
+		{
+			for (int i = 0; i < Indicators.Length; i++)
 			{
-				DebugMsg("IndicatorsObj is null");
-			}
-			Transform[] Indicators = IndicatorsObj.GetComponentsInChildren<Transform>();
-			if (Indicators != null)
-			{
-				for (int i = 0; i < Indicators.Length; i++)
+				GameObject Indicator = Indicators[i].gameObject;
+				Color tempColor = Color.black;
+				if (Indicator.name == "IndMainPower")
 				{
-					GameObject Indicator = Indicators[i].gameObject;
-					Color tempColor;
-					if (Indicator.name == "IndMainPower")
+					if (BlockIsPowered)
 					{
-						if (BlockIsPowered)
+						tempColor = Color.green;
+					}
+					else
+					{
+						tempColor = Color.black;								
+					}	
+				}
+				if(Indicator.name == "IndInput0")
+				{
+					if (inputStates[0])
+					{
+						if(inputStates[1] && inputStates[2])
 						{
 							tempColor = Color.green;
 						}
 						else
 						{
-							tempColor = Color.black;								
-						}	
-						SetIndicatorColor(Indicator, tempColor);
-						
-					}
-					//if (System.Text.RegularExpressions.Regex.Match(Indicator.name, @"Input").Success)
-					if(Indicator.name == "IndInput0")
-					{
-						if (inputStates[0])
-						{
-							tempColor = Color.yellow;
+							tempColor = Color.yellow;	
 						}
-						else
-						{
-							tempColor = Color.black;								
-						}	
-						SetIndicatorColor(Indicator, tempColor);
 					}
-					if(Indicator.name == "IndInput1")
+					else
 					{
-						if (inputStates[1])
-						{
-							tempColor = Color.yellow;
-						}
-						else
-						{
-							tempColor = Color.black;								
-						}	
-						SetIndicatorColor(Indicator, tempColor);						
-					}
-					if(Indicator.name == "IndInput2")
-					{
-						if (inputStates[2])
-						{
-							tempColor = Color.yellow;
-						}
-						else
-						{
-							tempColor = Color.black;								
-						}	
-						SetIndicatorColor(Indicator, tempColor);						
-					}
-					if (Indicator.name == "IndOutput")
-					{
-						if (inputStates[0] && BlockIsPowered)
-						{
-							tempColor = Color.blue;
-						}
-						else
-						{
-							tempColor = Color.black;								
-						}	
-						SetIndicatorColor(Indicator, tempColor);						
-					}
+						tempColor = Color.black;								
+					}	
 				}
+				if(Indicator.name == "IndInput1")
+				{
+					if (inputStates[1])
+					{
+						if(inputStates[0] && inputStates[2])
+						{
+							tempColor = Color.green;
+						}
+						else
+						{
+							tempColor = Color.yellow;	
+						}
+					}
+					else
+					{
+						tempColor = Color.black;								
+					}						
+				}
+				if(Indicator.name == "IndInput2")
+				{
+					if (inputStates[2])
+					{
+						if(inputStates[0] && inputStates[1])
+						{
+							tempColor = Color.green;
+						}
+						else
+						{
+							tempColor = Color.yellow;	
+						}
+					}
+					else
+					{
+						tempColor = Color.black;								
+					}					
+				}				
+				if (Indicator.name == "IndOutputMode")
+				{
+					if (outputMode)
+					{
+						tempColor = Color.blue;
+					}
+					else
+					{
+						tempColor = Color.black;								
+					}						
+				}
+				if (Indicator.name == "IndOutput")
+				{
+					if (BlockIsPowered && BlockIsTriggered)
+					{
+						tempColor = Color.green;
+					}
+					else
+					{
+						tempColor = Color.black;								
+					}						
+				}
+				SetIndicatorColor(Indicator, tempColor);
 			}
-		}		
-		return true;
+		}
 	}
+	
 	
 	static Vector3i[] GetInputLocations(Vector3i _blockPos, BlockValue _blockValue)
 	{
@@ -313,9 +355,9 @@ public class BlockLogicGateMain : BlockPowered
  
 	public bool[] GetInputStates(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue)
 	{
-		bool[] inputStatess = {false, false, false};
+		bool[] inputStates = {false, false, false};
 		Vector3i[] locations = GetInputLocations(_blockPos, _blockValue);
-		for(int i=0; i < inputStatess.Length; i++)
+		for(int i=0; i < inputStates.Length; i++)
 		{
 			BlockValue inputBlockValue = _world.GetBlock(locations[i]);
 			Type inputBlockType = Block.list[inputBlockValue.type].GetType();
@@ -326,99 +368,85 @@ public class BlockLogicGateMain : BlockPowered
 				{
 					if(tileEntityPowered.IsPowered)
 					{
-						inputStatess[i] = true;
+						inputStates[i] = true;
 					}
 				}
 			}
 		}
-		return inputStatess;
+		return inputStates;
 	}
 	
 
 	public override void OnBlockEntityTransformAfterActivated(WorldBase _world, Vector3i _blockPos, int _cIdx, BlockValue _blockValue, BlockEntityData _ebcd)
 	{
         DebugMsg("OnBlockEntityTransformAfterActivated");
-		this.XR(_world, _cIdx, _blockPos, _blockValue, false);
+		this.XR(_world, _cIdx, _blockPos, _blockValue);
 		base.OnBlockEntityTransformAfterActivated(_world, _blockPos, _cIdx, _blockValue, _ebcd);
-		/*
- 			bool flag = (_blockValue.meta & 1) != 0;
-			bool flag2 = (_blockValue.meta & 2) != 0;
-			if (_ebcd != null && _ebcd.transform != null && _ebcd.transform.gameObject != null)
-			{
-				Renderer[] componentsInChildren = _ebcd.transform.gameObject.GetComponentsInChildren<Renderer>();
-				if (componentsInChildren != null)
-				{
-					for (int i = 0; i < componentsInChildren.Length; i++)
-					{
-						if (componentsInChildren[i].material != componentsInChildren[i].sharedMaterial)
-						{
-							componentsInChildren[i].material = new Material(componentsInChildren[i].sharedMaterial);
-						}
-						if (flag)
-						{
-							componentsInChildren[i].material.SetColor("_EmissionColor", (!flag2) ? Color.red : Color.green);
-						}
-						else
-						{
-							componentsInChildren[i].material.SetColor("_EmissionColor", Color.black);
-						}
-						componentsInChildren[i].sharedMaterial = componentsInChildren[i].material;
-					}
-				}
-			}
-		*/
 	}
 
 	public override bool ActivateBlock(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, bool isOn, bool isPowered)
 	{
         DebugMsg("ActivateBlock");
 		bool isOn2 = false;
+		bool isOn3 = false;
+		bool[] inputStates = GetInputStates(_world, _cIdx, _blockPos, _blockValue);
+		bool outputMode =  getBitBool(_blockValue.meta, metaIndexOutMode);
 			
-		// Get call stack
-		StackTrace stackTrace = new StackTrace();
-		// Get calling method name
-		DebugMsg(String.Concat("ActivateBlock Caller1 ", stackTrace.GetFrame(1).GetMethod()));
-		DebugMsg(String.Concat("ActivateBlock Caller2 ", stackTrace.GetFrame(2).GetMethod()));
-		DebugMsg(String.Concat("ActivateBlock Caller3 ", stackTrace.GetFrame(3).GetMethod()));
-		DebugMsg(String.Concat("ActivateBlock Caller4 ", stackTrace.GetFrame(4).GetMethod()));
-		DebugMsg(String.Concat("ActivateBlock Caller5 ", stackTrace.GetFrame(5).GetMethod()));
-		DebugMsg(String.Concat("ActivateBlock Caller6 ", stackTrace.GetFrame(6).GetMethod()));
+		StackTrace st = new StackTrace(1, true);
+		StackFrame [] stFrames = st.GetFrames();
+		for(int i=0; i < stFrames.Length; i++ )
+		{
+		   DebugMsg(String.Concat("OnBlockActivated CallingMethod:",stFrames[i].GetMethod() ));
+		}
 		
 		DebugMsg(String.Concat("ActivateBlock isPowered=", isPowered ? "1" : "0"));
 		DebugMsg(String.Concat("ActivateBlock isOn=", isOn ? "1" : "0"));
-
-		bool[] inputStates = GetInputStates(_world, _cIdx, _blockPos, _blockValue);
 		DebugMsg(String.Concat("ActivateBlock inputStates=", inputStates[0] ? "1" : "0"));
+		DebugMsg(String.Concat("ActivateBlock outputMode=", outputMode ? "1" : "0"));
 		
-		if(inputStates[0] && isPowered){
-			// switch has 2nd input
+
+		bool inputOn = true;
+		for(int i = 0; i < inputStates.Length; i++)
+        if (!inputStates[i]) {
+            inputOn = false;
+            break;
+        }
+		
+		if(inputOn && isPowered){
 			isOn2 = true;
 		}
 		else
 		{
 			isOn2 = false;
 		}		
-		DebugMsg(String.Concat("ActivateBlock isOn2=", isOn2 ? "1" : "0"));
-		
+		DebugMsg(String.Concat("ActivateBlock isOn2=", isOn2 ? "1" : "0"));		
+					
+		if(outputMode)
+		{
+			isOn3 = !isOn2;
+		}
+		else
+		{
+			isOn3 = isOn2;
+		}
 
 		TileEntityPoweredTrigger tileEntityPoweredTrigger = _world.GetTileEntity(_cIdx, _blockPos) as 	TileEntityPoweredTrigger;
 		if (tileEntityPoweredTrigger != null)
 		{
-			if(isOn != isOn2)
+			if(isOn != isOn3)
 			{
-			tileEntityPoweredTrigger.IsTriggered = isOn2; //tileEntityPoweredTrigger.
+			tileEntityPoweredTrigger.IsTriggered = isOn3; //tileEntityPoweredTrigger.
 			
 			//tileEntityPoweredTrigger.Activate(isPowered, isOn2);
 			}
 			DebugMsg(String.Concat("ActivateBlock tileEntityPoweredTrigger.IsTriggered=", tileEntityPoweredTrigger.IsTriggered ? "1" : "0"));
 
-		}				
-						
+		}
+		_blockValue.meta = setBoolBit(_blockValue.meta, 0, isPowered); //(byte)(((int)_blockValue.meta & -2) | ((!isPowered) ? 0 : 1));
+		_blockValue.meta = setBoolBit(_blockValue.meta, 1, isOn3); //(byte)(((int)_blockValue.meta & -3) | ((!isOn3) ? 0 : 2));
 
-			_blockValue.meta = (byte)(((int)_blockValue.meta & -3) | ((!isOn2) ? 0 : 2));
-			_blockValue.meta = (byte)(((int)_blockValue.meta & -2) | ((!isPowered) ? 0 : 1));
-		this.XR(_world, _cIdx, _blockPos, _blockValue, false);
-		_world.SetBlockRPC(_cIdx, _blockPos, _blockValue);
+		_world.SetBlockRPC(_cIdx, _blockPos, _blockValue);		
+		this.XR(_world, _cIdx, _blockPos, _blockValue);
 		
 		return true;
 	}
@@ -427,7 +455,7 @@ public class BlockLogicGateMain : BlockPowered
 	{
         DebugMsg("OnBlockValueChanged");
 		base.OnBlockValueChanged(_world, _clrIdx, _blockPos, _oldBlockValue, _newBlockValue);
-		//this.XR(_world, _clrIdx, _blockPos, _newBlockValue, false);
+		//this.XR(_world, _clrIdx, _blockPos, _newBlockValue);
 		BlockEntityData _ebcd = ((World)_world).ChunkClusters[_clrIdx].GetBlockEntity(_blockPos);
 		//this.HR(_ebcd, BlockSwitch.IsSwitchOn(_newBlockValue.meta), _newBlockValue);
 	}
@@ -528,7 +556,6 @@ public class BlockLogicGateMain : BlockPowered
 	private void SetIndicatorColor(GameObject _obj, Color color)
 	{
 		Renderer rend =_obj.GetComponentInChildren<Renderer>();
-		//rend.EnableKeyword("_EMISSION");
 		rend.material.SetColor("_Emission", color);
 	}
 }
